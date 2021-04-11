a18c4797249653fa14c8a0136bfe5c8fd149be4c
SMARTIUM <dev@smartium.io>
2021-04-11 02:28:56 -0300

    Fonta Awesome

--- client/main.js
+++ client/main.js
@@ -3,9 +3,12 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import '@fortawesome/fontawesome-free/js/all.min'
+
 import './main.html';
 
-stateName = new ReactiveVar('&nbsp;')
+stateName = new ReactiveVar()
 
 Template.mapa.onRendered(() => {})
 
@@ -19,19 +22,16 @@ Template.mapa.events({
     'click .mapsvg-region' (e) {
         e.preventDefault()
         $('.mapsvg-region').css('fill', 'antiquewhite')
-            // let timer;
         let _stateName = $(e.target).attr('name')
         $(e.target).css('fill', 'orange')
         console.log(_stateName);
-        stateName.set(`<span class="clear">(X)</span> ${_stateName}`)
-            // timer = Meteor.setTimeout(function() {
-            //     stateName.set('&nbsp;')
-            // }, 5000);
+        stateName.set(_stateName)
+        $('.state-name-h').show()
     },
 
     'click .clear' (e) {
         e.preventDefault()
         $('.mapsvg-region').css('fill', 'antiquewhite')
-        stateName.set('&nbsp;')
+        $('.state-name-h').hide()
     }
 })
